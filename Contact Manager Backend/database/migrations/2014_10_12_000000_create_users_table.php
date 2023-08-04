7a577f0e41f0f8743ba143c53e6498ed154d6191
Rayan <rayanemad508@gmail.com>
2023-08-05 01:31:23 +0300

    BE : created migration

--- Contact Manager Backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ Contact Manager Backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,32 +1,40 @@
 <?php
 
+// Migration file: create_users_table.php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
+    public function up(){
+        
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('phone_number');
+            $table->decimal('latitude', 10, 7)->nullable();
+            $table->decimal('longitude', 10, 7)->nullable();
             $table->timestamps();
         });
+
+        Schema::create('user_contacts', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id_1');
+            $table->foreign('user_id_1')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id_2');
+            $table->foreign('user_id_2')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('user_contacts');
     }
-};
+}
