c8f2f84813a2a4c4ecae80b806590f9b6a098cb7
Rayan <rayanemad508@gmail.com>
2023-08-06 11:20:13 +0300

    FE : contact list container component

---
+++ frontend-cm/src/components/ContactListContainer/index.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import ContactForm from '../ContactForm'; 
+import ContactList from '../ContactCard'; 
+
+const ContactListContainer = () => {
+  const [contacts, setContacts] = useState([]);
+
+  const token = localStorage.getItem("token"); 
+
+
+  const fetchContactList = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/contact-list', {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        // Check for error response and handle it accordingly
+        const errorData = await response.json();
+        console.log('Error fetching contact list:', errorData.message);
+      } else {
+        const data = await response.json();
+        setContacts(data.contacts);
+      }
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  };
+
+  const addContact = async (newContact) => {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/create-contact', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify(newContact),
+      });
+
+      if (!response.ok) {
+        // Check for error response and handle it accordingly
+        const errorData = await response.json();
+        console.log('Error adding contact:', errorData.message);
+      } else {
+        const data = await response.json();
+        setContacts([...contacts, data.contact]);
+      }
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchContactList();
+  }, []);
+
+  return (
+    <div>
+      <h1>Contact List</h1>
+      <ContactForm addContact={addContact} />
+      <ContactList contacts={contacts} />
+    </div>
+  );
+};
+
+export default ContactListContainer;
